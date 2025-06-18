df2c61fc0a2985988526cf2e2b683c2806f19d39
Ankita <ankita180206@gmail.com>
2025-06-18 22:32:59 +0530

    Update main.py

--- Blog Capstone Project/id/main.py
+++ Blog Capstone Project/id/main.py
@@ -1,18 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 import requests
-posts = requests.get("https://api.npoint.io/88c2c1f644ef334058be").json()
-app= Flask(__name__,static_folder='static')
+import smtplib
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+my_email = "ankitamodak1806@gmail.com"
+my_pass = "kkgcykjkkhbouyup"
+app = Flask(__name__)
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com",587) as connection:
+        connection.starttls()
+        connection.login(my_email,my_pass)
+        connection.sendmail(my_email, my_email,email_message)
+
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html",all_posts = posts)
+    return render_template("index.html", all_posts=posts)
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact",methods=["POST","GET"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
 
 @app.route("/post/<int:index>")
 def show_post(index):
@@ -22,5 +40,6 @@ def show_post(index):
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
